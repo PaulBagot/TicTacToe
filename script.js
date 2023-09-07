db441e1a6c743d52035613ea66d7b17e0930bb24
Paul Bagot <bagotpa@gmail.com>
2023-09-07 13:35:40 +0200

    added the endGame function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let currentPlayerHTML = document.getElementById('current_player');
 let tableBody = document.getElementById('table_body');
 let currentPlayer;
+let table = [['', '', ''], ['', '', ''], ['', '', '']];
 let turn;
 
 /**
@@ -20,6 +21,8 @@ let initTictactoe = () => {
     turn = 1;
 }
 
+initTictactoe();
+
 /**
  * change the current player to the next player
  * @returns the current player's symbole
@@ -33,4 +36,22 @@ let nextPlayer = () => {
     return 'O';
 }
 
-initTictactoe();
+/**
+ * say if the game is finished
+ * @returns true if the game is finish
+ */
+let endGame = () => {
+    for(let i = 0; i < 3; i++) {
+        if(table[i][0] != '' && table[i][0] == table[i][1] && table[i][1] == table[i][2])
+            return true;
+    }
+    for(let i = 0; i < 3; i++) {
+        if(table[0][i] != '' && table[0][i] == table[1][i] && table[1][i] == table[2][i])
+            return true;
+    }
+    if(table[0][0] != '' && table[0][0] == table[1][1] && table[1][1] == table[2][2])
+        return true;
+    if(table[0][2] != '' && table[0][2] == table[1][1] && table[1][1] == table[2][0])
+        return true;
+    return turn > 9;
+}
