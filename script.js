e713408c09b199e5d0fc06fcda98773f170dae9a
Paul Bagot <bagotpa@gmail.com>
2023-09-07 13:33:14 +0200

    added the nextPlayer function

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let currentPlayerHTML = document.getElementById('current_player');
 let tableBody = document.getElementById('table_body');
-let currentPlayer = '';
+let currentPlayer;
 let turn;
 
+/**
+ * initialisation for the tictactoe game
+ */
 let initTictactoe = () => {
     let tableHTML = '';
     for(let i = 0; i < 3; i++) {
@@ -15,6 +18,19 @@ let initTictactoe = () => {
     tableBody.innerHTML += tableHTML;
     currentPlayer = 1;
     turn = 1;
+}
+
+/**
+ * change the current player to the next player
+ * @returns the current player's symbole
+ */
+let nextPlayer = () => {
+    if(currentPlayer == 1) {
+        currentPlayer = 2;
+        return 'X';
+    }
+    currentPlayer = 1;
+    return 'O';
 }
 
 initTictactoe();
